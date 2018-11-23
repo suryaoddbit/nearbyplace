5f9d550bd100893ede8d48510d21f1d19c87e9ed
I Wayan Surya Adi Yasa <suryaapps.dev@gmail.com>
2018-11-23 21:07:23 +0800

    use google maps npm pakage https://www.npmjs.com/package/@google/maps

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-(function () {
-    "use strict";
+var googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyDw8Gupgo_C4ut_LVuzG8cX1Pqk8RsaPF4'
+  });
 
-    var assert = require("assert");
-
-    var NearBySearch = require("googleplaces/lib/NearBySearch");
-    var config = require("./config.js");
-
-    var nearBySearch = new NearBySearch(config.apiKey, config.outputFormat);
-
-    var parameters = {
-        location: [-8.6863826, 115.2316578],
-        keyword: "indomaret"
-    };
-
-    nearBySearch(parameters, function (error, response) {
-        console.log(response)
-        if (error) throw error;
-        assert.notEqual(response.results.length, 0, "Place search must not return 0 results");
-    });
-
-})();
+  googleMapsClient.placesNearby({
+    keyword: 'indomaret',
+    location: {latitude: -8.6863826, longitude: 115.2316578},
+    radius: 1500
+  }, function(err, response) {
+    if (!err) {
+      console.log(response.json);
+    }
+  });
